988074800654431eea5e86ee4f474c50861a5f1e
Sean Sitter <sean.sitter@babycenter.com>
2015-11-04 11:24:59 -0800

    enables eureka

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,30 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+package com.bc.discovery.eurekacloud.microservice.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//@ComponentScan("com.bc.discovery.eurekacloud.microservice")
+//@EnableCircuitBreaker
+//@EnableFeignClients(basePackages = "com.bc.discovery.eurekacloud.microservice.client")
+@SpringBootApplication
+@EnableEurekaClient
 @RestController
-@EnableAutoConfiguration
-public class App {
+public class MicroserviceApplication {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceApplication.class, args);
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(App.class, args);
+    @RequestMapping(value = "/v1/list", method = RequestMethod.GET)
+    public String list()
+    {
+        return "hi there, sean -ok!";
     }
-
 }
