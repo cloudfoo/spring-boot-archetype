25c81398463ad598d5ea9a0f25008b61d7c36d64
Sean Sitter <sean.sitter@babycenter.com>
2015-11-04 12:22:28 -0800

    fixes deps for spring cloud

---
+++ src/main/java/app/App.java
@@ -0,0 +1,30 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//@EnableCircuitBreaker
+//@EnableFeignClients(basePackages = "com.bc.discovery.eurekacloud.microservice.client")
+@ComponentScan("app")
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping(value = "/v1/list", method = RequestMethod.GET)
+    public String list()
+    {
+        return "hi there, sean -ok!";
+    }
+}
